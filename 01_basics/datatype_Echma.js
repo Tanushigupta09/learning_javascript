313ea424dd6a3fe42be03ef34bf494bff82a0a8e
Tanushi Gupta <131553102+Tanushigupta09@users.noreply.github.com>
2024-01-19 14:36:50 +0000

    datatypes

---
+++ 01_basics/datatype_Echma.js
@@ -0,0 +1,18 @@
+"use strict"; //let our compiler compile our code in newer version of JS;
+
+// alert(3+3); // we can't use alert here because we are using it in node.js not on browser whenver we run alert we run it either binding it with html or run it in our browser console
+console.log("tanushi gupta");
+
+/* Primitive datatype
+number
+string
+boolean
+null=>it is a representation of empty value
+undefined=> means variable is declared but value is not given
+symbol=> uniqueness ke liye use karte hai
+*/
+//object is also a datatype
+
+console.log(typeof "tanushi");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined because it is a type in js
