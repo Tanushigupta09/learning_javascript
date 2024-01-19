364949bf69027055b98945ec7ca1f663ee2f3f80
Tanushi Gupta <131553102+Tanushigupta09@users.noreply.github.com>
2024-01-19 15:21:14 +0000

    datatypeconversion

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,61 @@
+// let score="55";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let value=Number(score);
+// console.log(typeof value);
+// console.log(value);
+
+// let score1=null;
+// console.log(typeof score1);
+// let value1=Number(score1);
+// console.log(typeof value1);
+// console.log(value1);
+
+// let score2=undefined;
+// console.log(typeof score2);
+// let value2=Number(score2);
+// console.log(typeof value2)
+// console.log(value2);
+
+// let score3= "09Tanushi";
+// console.log(typeof score3);
+// let value3=Number(score3);
+// console.log(typeof value3);
+// console.log(value3);
+
+// let score4= "";
+// console.log(typeof score4);
+// let value4=Number(score4);
+// console.log(typeof value4);
+// console.log(value4);
+
+let num1=88;
+let change1= String(num1);
+console.log(typeof change1);
+console.log(change1);
+
+
+
+
+let num2=null;
+let change2= String(num2);
+console.log(typeof change2);
+console.log(change2);
+
+
+/* datatype conversion 
+ initial datatype           on number conversion final datatype            
+ string                       number                                         
+ null                         0
+ undefine                     NAN
+ empty  string                0
+ */  
+/*
+ initial datatype           on boolean conversion final datatype     
+ 1                           true
+ 0                           false
+ ""                          false
+ "hitesh"                    true
+
+*/   
