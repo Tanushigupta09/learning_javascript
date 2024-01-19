145182c25ef7ce1a950f069daa78e7a4f4c6b60c
Tanushi Gupta <131553102+Tanushigupta09@users.noreply.github.com>
2024-01-19 13:46:08 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId=852963;
+
+let accountEmail="tanushi@gmail.com";
+
+var accountPassword="shrikrishn";
+
+accountCity= "Vrindavan"
+let account_State;
+
+accountCity="Barsana"
+// accountId=741852;
+console.log(accountId);
+
+accountEmail="tg@google.com";
+accountPassword="147852";
+console.table([accountEmail,accountPassword, account_State, accountCity]);
