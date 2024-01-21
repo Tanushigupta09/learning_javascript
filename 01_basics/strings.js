05b038978d4c06e1d82c87545c77ab189ff5494e
Tanushi Gupta <131553102+Tanushigupta09@users.noreply.github.com>
2024-01-21 14:14:05 +0000

    string completed

---
+++ 01_basics/strings.js
@@ -0,0 +1,28 @@
+// const name="Tanushi";
+// const dreams=22;
+// console.log(`My name is ${name} and my dreams count is ${dreams}`);
+
+const value="I love krishn and sriji and for me the meaning of love is specified by radhakrishn";
+
+// STRING METHODS
+
+// console.log(value.slice(10,16));
+
+// console.log(value.substring(-10,15));
+
+// console.log(value.substr(10,25));
+
+// console.log(value.replace("sriji","radha"));
+
+// console.log(value.replaceAll("krishn","krishna"));
+
+// console.log(value.toUpperCase());
+
+// let value1=" and krishn is my favourite bahle hi bahut test lete hai lekin harke jeetne wale ko hi toh badshah kehte hai";
+
+// console.log(value.concat(value1));
+
+// console.log(value.trim());
+
+// let value1=value.padStart(95,"radhe-radhe  ");
+// console.log(value1);
