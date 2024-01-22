eb2ff55f334737079f1b6a33164b8664d33cddf5
Tanushi Gupta <131553102+Tanushigupta09@users.noreply.github.com>
2024-01-22 17:44:00 +0000

    arrays done

---
+++ 02_basics/Arrays.js
@@ -0,0 +1,75 @@
+// Syntax1
+// const arr=[1,2,3,4,5];
+// console.log(arr);
+
+// Syntax2
+// const newArr= new Array(1,2,3,4,5);
+// console.log(newArr);
+
+// ARRAY METHODS
+
+// const arr=[1,2,3,4,5];
+// arr.push(6);
+// arr.push(7);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+// UNSHIFT METHODS
+// const arr=[8,9,7,6];
+// arr.unshift(10);
+// console.log(arr);
+
+// SHIFT METHODS
+// const arr=[8,9,7,6];
+// arr.shift();
+// console.log(arr);
+
+// INCLUDES 
+
+// const arr=["tanushi","hrishabh sir","saurabh sir","puneet rehlan sir"];
+// console.log(arr.includes("hrishabh sir"));
+
+// INDEX OF
+
+// const arr=["tanushi","hrishabh sir","saurabh sir","puneet rehlan sir"];
+// console.log(arr.indexOf("puneet rehlan sir"));
+
+
+// CONCAT OPERATOR
+//  const arr=["tanushi","hrishabh sir","saurabh sir","puneet rehlan sir"];
+//  const arr2=["sriji lover","krishna lover","mahadev lover","guru gobind lover"];
+//  const arr3=arr.concat(arr2);
+//  console.log(arr3);
+
+//  Join operator
+// const arr=["tanushi","hrishabh sir","saurabh sir","puneet rehlan sir"];
+// const arr2=arr.join();
+// console.log(arr2)
+
+// SPREAD OPERATOR
+
+// const arr=[1,2,3,4,5];
+// const arr2=[6,7,8,9];
+// const arr3=[...arr,...arr2];
+// console.log(arr3);
+
+// FLAT OPERATION
+// const arr=[1,2,3,4,5,[67,7,8],8,9,[10,27,48]];
+// const arr2= arr.flat(2);
+// console.log(arr2);
+
+// CONVERT STRING TO ARRAY
+
+// console.log(Array.from("Tanushi"));
+
+// console.log(Array.from({name:"tanushi"})); return empty array
+
+// let score1=100;
+// let score2=200
+// let score3=300
+// console.log(Array.of(score1,score2,score3));
+
+
+
+
